865e25235e2d40d90bc72c78be77bada5b01b389
Artem Pivrik <artempivrik90@gmail.com>
2019-03-16 02:59:43 +0200

    refactor code to class

--- parser.ts
+++ parser.ts
@@ -1,26 +1,48 @@
 import {parse} from 'papaparse';
 import {readFileSync} from 'fs';
 
-const file = readFileSync('./Users.csv', 'utf8');
-const csvData = parse(file, {
-        complete: (result) => result.data
-    }
-);
-export const csv = csvData.data.map((t) => {
-    return t.slice(0, 6)
-}).map((t: string[]) => {
-    return {...t}
-})
+class CsvData {
+    file: any;
+    resultArray: any;
+    utf: string;
 
-let propNames =csv[0];
-let resultArray = [];
-for (let i=1;i<csv.length;i++) {
-    let newItem = {};
-    let itemFromArray = csv[i];
-    Object.keys(propNames).forEach(key => {
-        newItem[propNames[key]] = itemFromArray[key];
-    });
-    resultArray.push(newItem);
+    constructor(file, utf) {
+        this.utf=utf;
+        this.file = readFileSync(file,this.utf);
+        this.resultArray = null;
+    }
+
+    private _parseData(): any {
+        const csvData = parse(this.file, {
+                complete: (result) => result.data
+            }
+        );
+        return csvData;
+    }
+
+    getCsvData(): any {
+
+        const csv = this._parseData().data.map((t) => {
+            return t.slice(0, 6)
+        }).map((t: string[]) => {
+            return {...t}
+        });
+
+        let propNames = csv[0];
+        this.resultArray = [];
+        for (let i = 1; i < csv.length; i++) {
+            let newItem = {};
+            let itemFromArray = csv[i];
+            Object.keys(propNames).forEach(key => {
+                newItem[propNames[key]] = itemFromArray[key];
+            });
+            this.resultArray.push(newItem);
+        }
+        return this.resultArray;
+    }
 }
 
-console.dir(resultArray)
+const scv = new CsvData('./Users.csv', 'utf8');
+export const data:Array<{}>= scv.getCsvData();
+
+console.dir(data);
